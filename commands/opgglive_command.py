edf0b0e82c1f1e391c2ab1af43e06b3c7e22e221
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2020-02-10 22:16:17 +0100

    yess

--- commands/opgglive_command.py
+++ commands/opgglive_command.py
@@ -32,7 +32,6 @@ class OPGGLIVE(commands.base_command.BaseCommand):
             FriendNames = []
             TierF = []
             Wrf = []
-            TotalF = []
 
             for i in l.findAll("a",{"class":"SummonerName"}): 
                 FriendNames.append(i.text)
@@ -43,15 +42,11 @@ class OPGGLIVE(commands.base_command.BaseCommand):
             for i in l.findAll("td",{"class":"RankedWinRatio Cell"}): 
                 Wrf.append(i.text)
 
-            for i in l.findAll("span",{"class":"TotalCount"}): 
-                TotalF.append(i.text)
-            
             l=soup.find("table",{"class":"Table Team-200"})
 
             EnemyNames = []
             TierE = []
             Wre = []
-            TotalE = []
 
             for i in l.findAll("a",{"class":"SummonerName"}): 
                 EnemyNames.append(i.text)
@@ -61,15 +56,12 @@ class OPGGLIVE(commands.base_command.BaseCommand):
             
             for i in l.findAll("td",{"class":"RankedWinRatio Cell"}): 
                 Wre.append(i.text)
-            
-            for i in l.findAll("span",{"class":"TotalCount"}): 
-                TotalE.append(i.text)
 
             l=soup.find("div",{"class":"Box"})
 
             GameMode=[]
             MapName=""
-            Current="04:41:12"
+            Current=""
 
             for i in l.findAll("div",{"class":"Title"}): 
                 GameMode.append(i.text)
@@ -77,8 +69,8 @@ class OPGGLIVE(commands.base_command.BaseCommand):
             for i in l.findAll("small",{"class":"MapName"}): 
                 MapName=i.text
             
-            # for i in l.findAll("small",{"class":"Time"}): 
-            #     Current=i.text
+            for i in l.findAll("small",{"class":"Time"}): 
+                Current=i.text
             
             Time=Current[len(Current)-8:len(Current)-3]
 
@@ -96,16 +88,16 @@ class OPGGLIVE(commands.base_command.BaseCommand):
             print("Error")
             await message.channel.send(message.author.mention + "\n"+"**Player is currently not in game.**")
 
-        await message.channel.send(#"**Gamemode: **"+GameMode[0][0:13]+"\t** Map: **"+MapName+"\n"  #"\t**Time: **"+TimeEUNE+"\n"
-            get_emoji(":small_blue_diamond:")+f"** "+FriendNames[0]+"**\t"+get_emoji(":military_medal:")+f"** Rank: **"+TierF[0].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wrf[0][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+"** Played Matches: **"+TotalF[0][0:len(TotalF[0])-6].lstrip().rstrip()+"\n"
-            +get_emoji(":small_blue_diamond:")+f"** "+FriendNames[1]+"**\t"+get_emoji(":military_medal:")+f"** Rank: **"+TierF[1].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wrf[1][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+TotalF[1][0:len(TotalF[1])-6].lstrip().rstrip()+"\n"
-            +get_emoji(":small_blue_diamond:")+f"** "+FriendNames[2]+"**\t"+get_emoji(":military_medal:")+f"** Rank: **"+TierF[2].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wrf[2][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+TotalF[2][0:len(TotalF[2])-6].lstrip().rstrip()+"\n"
-            +get_emoji(":small_blue_diamond:")+f"** "+FriendNames[3]+"**\t"+get_emoji(":military_medal:")+f"** Rank: **"+TierF[3].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wrf[3][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+TotalF[3][0:len(TotalF[3])-6].lstrip().rstrip()+"\n"
-            +get_emoji(":small_blue_diamond:")+f"** "+FriendNames[4]+"**\t"+get_emoji(":military_medal:")+f"** Rank: **"+TierF[4].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wrf[4][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+TotalF[4][0:len(TotalF[4])-6].lstrip().rstrip()+"\n"
+        await message.channel.send("**Gamemode: **"+GameMode[0][0:13]+"\t** Map: **"+MapName+"\t**Time: **"+TimeEUNE+"\n"
+            +get_emoji(":small_blue_diamond:")+f"** "+FriendNames[0]+"**\t"+get_emoji(":military_medal:")+f"** Rank: **"+TierF[0].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wrf[0][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+"** Played Matches: **"+Wrf[0][9:15].strip()+"\n"
+            +get_emoji(":small_blue_diamond:")+f"** "+FriendNames[1]+"**\t"+get_emoji(":military_medal:")+f"** Rank: **"+TierF[1].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wrf[1][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+Wrf[1][9:15].strip()+"\n"
+            +get_emoji(":small_blue_diamond:")+f"** "+FriendNames[2]+"**\t"+get_emoji(":military_medal:")+f"** Rank: **"+TierF[2].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wrf[2][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+Wrf[2][9:15].strip()+"\n"
+            +get_emoji(":small_blue_diamond:")+f"** "+FriendNames[3]+"**\t"+get_emoji(":military_medal:")+f"** Rank: **"+TierF[3].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wrf[3][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+Wrf[3][9:15].strip()+"\n"
+            +get_emoji(":small_blue_diamond:")+f"** "+FriendNames[4]+"**\t"+get_emoji(":military_medal:")+f"** Rank: **"+TierF[4].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wrf[4][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+Wrf[4][9:15].strip()+"\n"
             +get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+get_emoji(":heavy_minus_sign:")+"\n"
-            +get_emoji(":small_orange_diamond:")+f" **"+EnemyNames[0]+"**\t"+get_emoji(":military_medal:")+f" **Rank: **"+TierE[0].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wre[0][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+TotalE[0][0:len(TotalE[0])-6].lstrip().rstrip()+"\n"
-            +get_emoji(":small_orange_diamond:")+f" **"+EnemyNames[1]+"**\t"+get_emoji(":military_medal:")+f" **Rank: **"+TierE[1].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wre[1][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+TotalE[1][0:len(TotalE[1])-6].lstrip().rstrip()+"\n"
-            +get_emoji(":small_orange_diamond:")+f" **"+EnemyNames[2]+"**\t"+get_emoji(":military_medal:")+f" **Rank: **"+TierE[2].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wre[2][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+TotalE[2][0:len(TotalE[2])-6].lstrip().rstrip()+"\n"
-            +get_emoji(":small_orange_diamond:")+f" **"+EnemyNames[3]+"**\t"+get_emoji(":military_medal:")+f" **Rank: **"+TierE[3].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wre[3][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+TotalE[3][0:len(TotalE[3])-6].lstrip().rstrip()+"\n"
-            +get_emoji(":small_orange_diamond:")+f" **"+EnemyNames[4]+"**\t"+get_emoji(":military_medal:")+f" **Rank: **"+TierE[4].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wre[4][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+TotalE[4][0:len(TotalE[4])-6].lstrip().rstrip()+"\n"
+            +get_emoji(":small_orange_diamond:")+f" **"+EnemyNames[0]+"**\t"+get_emoji(":military_medal:")+f" **Rank: **"+TierE[0].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wre[0][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+Wre[0][9:15].strip()+"\n"
+            +get_emoji(":small_orange_diamond:")+f" **"+EnemyNames[1]+"**\t"+get_emoji(":military_medal:")+f" **Rank: **"+TierE[1].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wre[1][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+Wre[1][9:15].strip()+"\n"
+            +get_emoji(":small_orange_diamond:")+f" **"+EnemyNames[2]+"**\t"+get_emoji(":military_medal:")+f" **Rank: **"+TierE[2].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wre[2][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+Wre[2][9:15].strip()+"\n"
+            +get_emoji(":small_orange_diamond:")+f" **"+EnemyNames[3]+"**\t"+get_emoji(":military_medal:")+f" **Rank: **"+TierE[3].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wre[3][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+Wre[3][9:15].strip()+"\n"
+            +get_emoji(":small_orange_diamond:")+f" **"+EnemyNames[4]+"**\t"+get_emoji(":military_medal:")+f" **Rank: **"+TierE[4].lstrip().rstrip()+"\t"+get_emoji(":trophy:")+f" **WinRate: **"+Wre[4][0:8].lstrip().rstrip()+"\t"+get_emoji(":hourglass:")+f"** Played Matches: **"+Wre[4][9:15].strip()+"\n"
         )
