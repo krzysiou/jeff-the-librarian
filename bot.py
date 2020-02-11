6e27a65e88e4cf04f066abcb849c3fa3a559be0b
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2020-02-11 20:52:46 +0100

    yes

--- bot.py
+++ bot.py
@@ -68,8 +68,8 @@ def main():
 
     @client.event
     async def on_message(message):
-        if str(message.author.tag) in settings.BLACKLIST:
-            await message.channel.send(message.author.mention + settings.QUOTES[random.randrange(0,3)])
+        if str(message.author) in settings.BLACKLIST:
+            await message.channel.send(message.author.mention + settings.QUOTES[random.randrange(0,2)])
         else:
             await common_handle_message(message)
 
