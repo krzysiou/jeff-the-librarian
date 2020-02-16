85f7875567d55066a878b34a5252432836cf8e7e
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2020-02-16 12:41:07 +0100

    g

--- commands/stalk_command.py
+++ commands/stalk_command.py
@@ -89,7 +89,7 @@ class Stalk(commands.base_command.BaseCommand):
             l=soup.find("ul",{"class":"Content"})
             RoleArray=[]
 
-            for i in l.findAll("div"): 
+            for i in l.findAll("div",{"class":"Name"}): 
                 RoleArray.append(i.text)
 
         else: 
@@ -98,15 +98,11 @@ class Stalk(commands.base_command.BaseCommand):
 
         print(RoleArray[0])
         print(RoleArray[1])
-        print(RoleArray[2])
-        print(RoleArray[3])
-        print(RoleArray[4])
-        print(RoleArray[5])
 
         await message.channel.send(
             get_emoji(":trophy:")+f"** Nickname: **" + Name + "\n"+
             get_emoji(":small_orange_diamond:")+f"** Solo/Duo Rank: **"+SoloDuoRank[1].strip()+"\t"+get_emoji(":small_orange_diamond:")+f"** Points: **" + LpSoloDuo[0].strip()+"\t"+get_emoji(":small_orange_diamond:")+f"** Win Rate: **" + SoloDuoWR[len(SoloDuoWR)-4:len(SoloDuoWR)]+"\n"+
             get_emoji(":small_orange_diamond:")+f"** Flex Rank: **" +FlexRank[2].strip()+"\t"+get_emoji("::small_orange_diamond:")+f"** Points: **" + FlexLp+"\t"+get_emoji("::small_orange_diamond:")+f"** Win Rate: **" + FlexWR[len(FlexWR)-4:len(FlexWR)]+"\n"+
-            get_emoji(":small_orange_diamond:")+f"** Main Role: **" +RoleArray[2]+"\n"+
-            get_emoji(":small_orange_diamond:")+f"** Sub Role: **" +RoleArray[5]+"\n"
+            get_emoji(":small_orange_diamond:")+f"** Main Role: **" +RoleArray[0]+"\n"+
+            get_emoji(":small_orange_diamond:")+f"** Sub Role: **" +RoleArray[1]+"\n"
         )
