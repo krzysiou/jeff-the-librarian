bd9403a63521c3e6698fb2116277476ef736d9b0
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2020-02-16 13:08:10 +0100

    www

--- commands/stalk_command.py
+++ commands/stalk_command.py
@@ -84,25 +84,25 @@ class Stalk(commands.base_command.BaseCommand):
         else: 
             print("Error")
 
-        #ROLE / 
-        if resp.status_code==200: 
-            l=soup.find("td",{"class":"PositionStats"})
-            RoleArray=[]
+        # #ROLE / 
+        # if resp.status_code==200: 
+        #     l=soup.find("td",{"class":"PositionStats"})
+        #     RoleArray=[]
 
-            for i in l.findAll("div",{"class":"PositionStatContent"}): 
-                RoleArray.append(i.text)
+        #     for i in l.findAll("div",{"class":"PositionStatContent"}): 
+        #         RoleArray.append(i.text)
 
-        else: 
-            print("Error")
+        # else: 
+        #     print("Error")
 
 
-        print(RoleArray[0])
-        print(RoleArray[1])
+        # print(RoleArray[0])
+        # print(RoleArray[1])
 
         await message.channel.send(
             get_emoji(":trophy:")+f"** Nickname: **" + Name + "\n"+
             get_emoji(":small_orange_diamond:")+f"** Solo/Duo Rank: **"+SoloDuoRank[1].strip()+"\t"+get_emoji(":small_orange_diamond:")+f"** Points: **" + LpSoloDuo[0].strip()+"\t"+get_emoji(":small_orange_diamond:")+f"** Win Rate: **" + SoloDuoWR[len(SoloDuoWR)-4:len(SoloDuoWR)]+"\n"+
-            get_emoji(":small_orange_diamond:")+f"** Flex Rank: **" +FlexRank[2].strip()+"\t"+get_emoji("::small_orange_diamond:")+f"** Points: **" + FlexLp+"\t"+get_emoji("::small_orange_diamond:")+f"** Win Rate: **" + FlexWR[len(FlexWR)-4:len(FlexWR)]+"\n"+
-            get_emoji(":small_orange_diamond:")+f"** Main Role: **" +RoleArray[0]+"\n"+
-            get_emoji(":small_orange_diamond:")+f"** Sub Role: **" +RoleArray[1]+"\n"
+            get_emoji(":small_orange_diamond:")+f"** Flex Rank: **" +FlexRank[2].strip()+"\t"+get_emoji("::small_orange_diamond:")+f"** Points: **" + FlexLp+"\t"+get_emoji("::small_orange_diamond:")+f"** Win Rate: **" + FlexWR[len(FlexWR)-4:len(FlexWR)]+"\n"
+            # +get_emoji(":small_orange_diamond:")+f"** Main Role: **" +RoleArray[0]+"\n"+
+            # get_emoji(":small_orange_diamond:")+f"** Sub Role: **" +RoleArray[1]+"\n"
         )
