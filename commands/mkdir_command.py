c886531be22eb076934de09ea57c151a12f97c14
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2020-02-17 18:04:30 +0100

    ww

---
+++ commands/mkdir_command.py
@@ -0,0 +1,23 @@
+import commands.base_command
+import requests 
+from bs4 import BeautifulSoup
+from utils import get_emoji
+import os
+
+class Stalk(commands.base_command.BaseCommand):
+
+    def __init__(self):
+        description = "Displays OPGG info of given summoner (use underscore instead of spaces)"
+        params = ["nickname"]
+        super().__init__(description, params)
+
+    async def handle(self, params, message, client):
+        try:
+            name = str(params[0])
+        except ValueError:
+            await message.channel.send("Please, provide valid numbers")
+            return
+
+        route = "/users/krzysiektluszcz/Desktop/" + name
+        if name != "":
+            os.mkdir(route)
