65dfdf837ea29e33ab251398923fb4612750a969
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2020-05-07 15:11:20 +0200

    spotify test 2

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ def main():
         if str(message.author.id) in settings.BLACKLIST:
             await message.channel.send(settings.QUOTES[random.randrange(0,6)]+"\n"+message.author.mention)
             await common_handle_message(message)
-        if (message.content.startswith("'spotify")) and (message.author.id not in settings.ADMIN):
+        if message.content.startswith("'spotify") and str(message.author.id) not in settings.ADMIN:
             await message.channel.send("Nie masz odpowiednich uprawnień by uywać tej komendy.\n" + message.author.mention)
         else:
             await common_handle_message(message)
