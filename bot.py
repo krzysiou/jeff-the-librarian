20c8d08d63f6aa0da40cf6ea8a3714d100f2b3bb
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2020-06-26 20:06:18 +0200

    lol

--- bot.py
+++ bot.py
@@ -74,9 +74,9 @@ def main():
     async def on_voice_state_update(user, before, after):
         if str(user.id) in settings.BLACKLIST:
             print(user.voice.channel.id)
-            if str(user.voice.channel.id) == "676571346733629498" and before.mute == False:
+            if str(user.voice.channel.id) == "676571346733629498" and before.mute == False and str(before.channel.id) != "676571346733629498":
                 await user.edit(reason=None,mute=True)
-            if str(user.voice.channel.id) != "676571346733629498" and before.mute == True:
+            if str(user.voice.channel.id) != "676571346733629498" and before.mute == True and str(before.channel.id) == "676571346733629498":
                 await user.edit(reason=None,mute=False)
     @client.event
     async def on_message_edit(before, after):
