ab4c6469f372b23b6136818b4058fea7756ffe24
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2020-06-26 19:30:39 +0200

    update

--- bot.py
+++ bot.py
@@ -74,7 +74,7 @@ def main():
     async def on_voice_state_update(user, before, after):
         if str(user.id) in settings.BLACKLIST:
             if user.voice.channel.id == "676571346733629498":
-                user.voice.mute = True
+                user.setMute(True, 'Sory Jobo')
 
     @client.event
     async def on_message_edit(before, after):
