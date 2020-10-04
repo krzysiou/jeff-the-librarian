64654e470074399a5eca9e4cb1ecc7e3d9d3c9b2
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2020-10-04 15:21:02 +0200

    update

--- bot.py
+++ bot.py
@@ -83,7 +83,7 @@ def main():
     async def on_message_edit(before, after):
         await common_handle_message(after)
 
-    client.run(settings.BOT_TOKEN)
+    client.start(settings.BOT_TOKEN)
 
 if __name__ == "__main__":
     main()
